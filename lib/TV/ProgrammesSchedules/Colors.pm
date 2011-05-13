b8853114aa8f06f339941806a8f7e7057f91de99
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-05-13 09:09:59 +0000

    Import of MANWAR/TV-ProgrammesSchedules-Colors-0.03 from CPAN.
    gitpan-cpan-distribution: TV-ProgrammesSchedules-Colors
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MANWAR/TV-ProgrammesSchedules-Colors-0.03.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/TV/ProgrammesSchedules/Colors.pm
+++ lib/TV/ProgrammesSchedules/Colors.pm
@@ -21,11 +21,11 @@ TV::ProgrammesSchedules::Colors - Interface to Colors TV Programmes Schedules.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 Readonly my $BASE_URL =>
 {
     'in'   => 'http://www.colorstv.in/schedule',
